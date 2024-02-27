f018b9f1d0a5777678ddd83f999e4c34bd5c06b3
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-02-27 10:10:17 +0800

    fix(highlight): 高亮元素被删除后再次输入颜色为清除

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,39 @@
-import { addEventListener, createRange, createStyle, getActiveText, setStyle, getActiveTextEditorLanguageId, getConfiguration, getPosition } from '@vscode-use/utils'
+import { addEventListener, createRange, createStyle, getActiveText, getActiveTextEditorLanguageId, getConfiguration, getPosition, isDark, setStyle } from '@vscode-use/utils'
 import type { Disposable, ExtensionContext } from 'vscode'
 
 export async function activate(context: ExtensionContext) {
   const disposes: Disposable[] = []
 
-  const userConfigurationStyle = getConfiguration('vscode-highlight-v-if.style')
-  const style = createStyle(userConfigurationStyle)
-  // const setStyle = (style: any, range: any) => {
-  //   const activeTextEditor = getActiveTextEditor()
-  //   if (!activeTextEditor)
-  //     return
-
-  //   const rangeOrOptins = range
-  //     ? (range as any).length
-  //         ? range
-  //         : [range]
-  //     : []
-  //   return activeTextEditor.setDecorations(style, rangeOrOptins as any[])
-  // }
-  const updateVIfStyle = () => {
+  const userConfigurationStyle = getConfiguration('vscode-vue-highlight.rules')[isDark() ? 'dark' : 'light']
+  const clearStyle: (() => void)[] = []
+  const updateVStyle = () => {
     const isVue = getActiveTextEditorLanguageId() === 'vue'
     const text = getActiveText()
+    clearStyle.forEach(cb => cb())
+    clearStyle.length = 0
     if (!text || !isVue)
       return
     const ranges: any = []
-    for (const matcher of text.matchAll(/(v-if|v-else-if|v-else)/g)) {
-      const start = matcher.index!
-      const end = start + matcher[0].length
-      const range = createRange(getPosition(start), getPosition(end))
-      ranges.push(range)
+    let style: any
+    for (const color in userConfigurationStyle) {
+      const option = userConfigurationStyle[color]
+      style = createStyle({ color })
+      if (option.length) {
+        const reg = new RegExp(option.join('|'), 'g')
+        for (const matcher of text.matchAll(reg)) {
+          const start = matcher.index!
+          const end = start + matcher[0].length
+          const range = createRange(getPosition(start), getPosition(end))
+          ranges.push(range)
+        }
+        setStyle(style, ranges)
+        clearStyle.push(() => setStyle(style))
+      }
     }
-    setStyle(style, ranges)
   }
-  updateVIfStyle()
-  disposes.push(addEventListener('text-change', updateVIfStyle))
-  disposes.push(addEventListener('activeText-change', updateVIfStyle))
+  updateVStyle()
+  disposes.push(addEventListener('text-change', updateVStyle))
+  disposes.push(addEventListener('activeText-change', updateVStyle))
   context.subscriptions.push(...disposes)
 }
 
